2008e78092df1eab9d5bfd519d7e867c06ccb132
diegokbalz8 <diegocabalceta8@gmail.com>
2024-04-26 21:41:20 -0400

    task 5 & 6

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,10 +4,29 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+const doesExist = (username)=>{
+  let userswithsamename = users.filter((user)=>{
+    return user.username === username
+  });
+  if(userswithsamename.length > 0){
+    return true;
+  } else {
+    return false;
+  }
+}
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+    const password = req.body.password;
+    if (username && password) {
+      if (!doesExist(username)) { 
+        users.push({"username":username,"password":password});
+        return res.status(200).json({message: "User successfully registred. Now you can login"});
+      } else {
+        return res.status(404).json({message: "User already exists!"});    
+      }
+    } 
+    return res.status(404).json({message: "Unable to register user."});
 });
 
 // Get the book list available in the shop
@@ -50,8 +69,13 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+//Write your code here
+const isbn = req.params.isbn
+
+//let filtered_users = users.filter((user) => user.isbn === isbn);
+res.send(JSON.stringify(books[isbn].reviews,null,4));
+  
+//return res.status(300).json({message: "Yet to be implemented"});
 });
 
 module.exports.general = public_users;
